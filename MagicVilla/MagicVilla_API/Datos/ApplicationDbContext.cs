8adc6c8503579f63a4916c03321a1c12a139b671
unknown <julian.herrera@accenture.com>
2024-06-03 16:05:07 -0500

    creando cadena de conexion a la base de datos

---
+++ MagicVilla/MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- MagicVilla/MagicVilla_API/Modelos/Villa.cs
+++ MagicVilla/MagicVilla_API/Modelos/Villa.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace MagicVilla_API.Modelos
 {
     public class Villa
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Nombre { get; set; }
 
